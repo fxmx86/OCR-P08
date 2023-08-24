79cb96c2ece81a5c29c4308fd056e5b71ec12688
François Xavier AMPE <FX.Ampe@octime.com>
2023-08-24 16:09:13 +0200

    Modif Flask

--- app.py
+++ app.py
@@ -43,61 +43,6 @@ def generate_img_from_mask(mask, palette=['gray', 'purple', 'black', 'steelblue'
 
     return img
 
-
-#def generate_img_from_mask(mask, palette=['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']):
-    '''Generate a color image array from a segmented mask
-    Args:
-      mask - numpy array of dimension
-      palette - colors list to be assigned to each class
-    Returns
-      Image array'''
-
-    CAT_DICT = {0: 'void', 1: 'flat', 2: 'construction', 3: 'object',
-                4: 'nature', 5: 'sky', 6: 'human', 7: 'vehicle'}
-
-    palette_rgb = [ (0.5019607843137255, 0.5019607843137255, 0.5019607843137255),
-                   (0.5019607843137255, 0.0, 0.5019607843137255),
-                   (0.0, 0.0, 0.0),
-                   (1.0, 0.6470588235294118, 0.0),
-                   (0.4196078431372549, 0.5568627450980392, 0.13725490196078433),
-                   (0.27450980392156865, 0.5098039215686274, 0.7058823529411765),
-                   (1.0, 0.0, 0.0),
-                   (0.0, 0.0, 0.803921568627451) ]
-
-    # Initializing the output image
-    img = np.zeros((mask.shape[0], mask.shape[1], 3), dtype='float')
-
-    # Assigning RGB channels
-    for cat in CAT_DICT.keys():
-        img[:, :, 0] += mask[:, :, cat] * palette_rgb[cat][0]
-        img[:, :, 1] += mask[:, :, cat] * palette_rgb[cat][1]
-        img[:, :, 2] += mask[:, :, cat] * palette_rgb[cat][2]
-
-    return img
-
-#def generate_img_from_mask(mask, palette=['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']):
-def generate_img_from_mask(mask, palette=['gray', 'purple', 'black', 'orange', 'olivedrab', 'steelblue', 'red', 'mediumblue']):
-    '''Generate a color image array from a segmented mask
-    Args:
-      mask - numpy array of dimension
-      palette - colors list to be assigned to each class
-    Returns
-      Image array'''
-
-    CAT_DICT = {0: 'void', 1: 'flat', 2: 'construction', 3: 'object',
-                4: 'nature', 5: 'sky', 6: 'human', 7: 'vehicle'}
-
-    # Initializing the output image
-    img = np.zeros((mask.shape[0], mask.shape[1], 3), dtype='float')
-
-    # Assigning RGB channels
-    for cat in CAT_DICT.keys():
-        img[:, :, 0] += mask[:, :, cat] * colors.to_rgb(palette[cat])[0]
-        img[:, :, 1] += mask[:, :, cat] * colors.to_rgb(palette[cat])[1]
-        img[:, :, 2] += mask[:, :, cat] * colors.to_rgb(palette[cat])[2]
-
-    return img
-
 def predict_segmentation(image_array, image_width, image_height):
     '''Generate a color mask from a model
     Args:
